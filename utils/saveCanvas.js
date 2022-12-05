a93bbe9280761f6f27d44234655863a1524ba8c9
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-12-05 17:29:48 -0500

    jpeg

--- utils/saveCanvas.js
+++ utils/saveCanvas.js
@@ -12,6 +12,6 @@ function saveArtwork() {
 	var dayoftheweek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 	var d = new Date();
 	var datestring = dayoftheweek[d.getDay()] + '_' + d.getDate() + '_' + d.getMonth() + '_' + d.getFullYear() + '_' + d.getHours() + '_' + d.getMinutes() + '_' + d.getSeconds();
-	var fileName = datestring + '.png';
+	var fileName = datestring + '.jpeg';
 	save(fileName);
 }
