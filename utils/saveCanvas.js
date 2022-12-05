5daf848bcb7194dd15e7b6f1fd7f9218a5384ab8
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-12-05 17:28:10 -0500

    added day of the week

--- utils/saveCanvas.js
+++ utils/saveCanvas.js
@@ -9,6 +9,9 @@ function keyPressed() {
 
 // make a function to save the canvas as a png file with the git branch name and a timestamp
 function saveArtwork() {
-	var timestamp = Date.now();
-	save('artwork' + '-' + timestamp + '.png');
+	var dayoftheweek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+	var d = new Date();
+	var datestring = dayoftheweek[d.getDay()] + '_' + d.getDate() + '_' + d.getMonth() + '_' + d.getFullYear() + '_' + d.getHours() + '_' + d.getMinutes() + '_' + d.getSeconds();
+	var fileName = datestring + '.png';
+	save(fileName);
 }
