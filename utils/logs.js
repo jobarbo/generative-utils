e9f3fa530eb9c0eb2b9674821fcc37fb93859b1e
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2022-12-19 17:07:18 -0500

    added logs to main lib

---
+++ utils/logs.js
@@ -0,0 +1,19 @@
+//* CONSOLE LOGGING *//
+
+function generateConsoleLogs(params) {
+	//* UNPACK PARAMETERS *//
+	// unpacking parameters we need in main.js and turning them into globals
+	for (var key in params) {
+		window[key] = params[key];
+	}
+	var jbarbeau_logo = '%c                                                                              \n' + '%c     Art by Jonathan Barbeau and Melivoros  |  { @jbarbeau.art }  |  2022     \n' + '%c                                                                              \n';
+
+	console.log(jbarbeau_logo, 'color: white; background: #000000; font-weight: bold; font-family: "Courier New", monospace;', 'color: white; background: #000000; font-weight: bold; font-family: "Courier New", monospace;', 'color: white; background: #000000; font-weight: bold; font-family: "Courier New", monospace;');
+	console.log('%cLa nuit porte... de garage\n', 'font-style: italic; font-family: "Courier New", monospace;');
+
+	// console table all params with their values
+	console.table('%cTOKEN FEATURES', 'color: white; background: #000000;', '\n', params);
+
+	console.log('%cCONTROLS', 'color: white; background: #000000;', '\n', 'cmd + s   : save artwork with date', '\n');
+}
+//* END CONSOLE LOGGING *//
