aadf855648138bdd846febe5767efbd195f57a1d
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-03-12 22:30:09 -0400

    updated texture lib

--- utils/smudge.js
+++ utils/smudge.js
@@ -1,30 +1,56 @@
 class Smudge {
-	constructor(rdnX, rdnY, w1, hue) {
-		this.xoff = 0;
-		this.yoff = 1;
-		this.woff1 = 0;
+	constructor(rdnX, rdnY, w1, color) {
+		this.xoff = random(1000000);
+		this.yoff = random(1000000);
+		this.woff1 = random(1000000);
+		this.aoff = random(1000000);
 		this.rdnX = rdnX;
 		this.rdnY = rdnY;
 		this.rdnW1 = w1;
-		this.mapXLow = -width / 3;
-		this.mapXHigh = width * 1.5;
-		this.mapYLow = -height / 3;
-		this.mapYHigh = height * 1.5;
-		this.hue = hue;
-		this.alpha = int(random(0, 15));
+		this.mapXLow = -width * 3;
+		this.mapXHigh = width * 3;
+		this.mapYLow = -height * 3;
+		this.mapYHigh = height * 3;
+		this.alpha = int(random(0, 100));
+		this.hue = hue(color);
+		this.sat = saturation(color);
+		this.bri = brightness(color);
+
+		this.xoffInc = width / 10000000;
+		this.yoffInc = width / 10000000;
+		this.woff1Inc = width / 400000;
+		this.aoffInc = width / 400000;
+
+		this.maxWidth = height / 1000;
+		this.minWidth = height / 5000;
+
+		this.x = rdnX;
+		this.y = rdnY;
+		this.w = map(noise(this.woff1), 0, 1, this.minWidth, this.maxWidth, true);
+
+		this.offset = this.w / 2;
 	}
 
 	display() {
-		this.xoff += width / 26666.67;
-		this.yoff += width / 100000;
-		this.woff1 += width / 1454.55;
+		this.xoff += this.xoffInc;
+		this.yoff += this.yoffInc;
+		this.woff1 += this.woff1Inc;
+		this.aoff += this.aoffInc;
 
-		const w1 = map(noise(this.woff1 + this.rdnW1), 0, 1, 0, width / 800);
-		const x = map(noise(this.xoff + this.rdnX), 0, 1, this.mapXLow, this.mapXHigh);
-		const y = map(noise(this.yoff + this.rdnY), 0, 1, this.mapYLow, this.mapYHigh);
+		this.w = map(noise(this.woff1), 0, 1, this.minWidth, this.maxWidth, true);
+		this.x = map(noise(this.xoff), 0, 1, this.mapXHigh, this.mapXLow, true);
+		this.y = map(noise(this.yoff), 0, 1, this.mapYHigh, this.mapYLow, true);
+		this.alpha = map(noise(this.woff1), 0.6, 1, 0, 50, true);
 
-		fill(this.hue, 10, 100, this.alpha);
+		this.offset = this.w / 2;
 		noStroke();
-		rect(x, y, w1, w1);
+		fill(this.hue, 0, 0, this.alpha / 2);
+		ellipse(this.x - this.offset, this.y - this.offset, this.w, this.w);
+		fill(0, 100, 100, this.alpha);
+		ellipse(this.x - this.offset, this.y + this.offset, this.w, this.w);
+		fill(200, 100, 100, this.alpha);
+		ellipse(this.x + this.offset, this.y - this.offset, this.w, this.w);
+		fill(this.hue, 0, 100, this.alpha);
+		ellipse(this.x + this.offset, this.y + this.offset, this.w * 1.25, this.w * 1.25);
 	}
 }
