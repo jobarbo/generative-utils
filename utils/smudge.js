eed41899e16fdf70dcc13e7b3a603450dcedc4d9
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-03-15 12:16:15 -0400

    code optimisation

--- utils/smudge.js
+++ utils/smudge.js
@@ -15,23 +15,43 @@ class Smudge {
 		this.hue = hue(color);
 		this.sat = saturation(color);
 		this.bri = brightness(color);
-
 		this.xoffInc = width / 10000000;
 		this.yoffInc = width / 10000000;
 		this.woff1Inc = width / 4000;
 		this.aoffInc = width / 400000;
-
 		this.maxWidth = height / 1000;
 		this.minWidth = height / 5000;
-
 		this.x = rdnX;
 		this.y = rdnY;
 		this.w = map(noise(this.woff1), 0, 1, this.minWidth, this.maxWidth, true);
-
 		this.offset = this.w / 2;
 	}
 
 	display() {
+		this.updateValues();
+
+		noStroke();
+
+		const ellipseConfigs = [
+			{x: this.x - this.offset, y: this.y - this.offset, w: this.w, h: this.w, col: [this.hue, 0, 0, this.alpha / 2]},
+			{x: this.x - this.offset, y: this.y + this.offset, w: this.w, h: this.w, col: [0, 100, 100, this.alpha]},
+			{x: this.x + this.offset, y: this.y - this.offset, w: this.w, h: this.w, col: [200, 100, 100, this.alpha]},
+			{
+				x: this.x + this.offset,
+				y: this.y + this.offset,
+				w: this.w * 1.25,
+				h: this.w * 1.25,
+				col: [this.hue, 0, 100, this.alpha],
+			},
+		];
+
+		ellipseConfigs.forEach(({x, y, w, h, col}) => {
+			fill(...col);
+			ellipse(x, y, w, h);
+		});
+	}
+
+	updateValues() {
 		this.xoff += this.xoffInc;
 		this.yoff += this.yoffInc;
 		this.woff1 += this.woff1Inc;
@@ -41,16 +61,6 @@ class Smudge {
 		this.x = map(noise(this.xoff), 0, 1, this.mapXHigh, this.mapXLow, true);
 		this.y = map(noise(this.yoff), 0, 1, this.mapYHigh, this.mapYLow, true);
 		this.alpha = map(noise(this.woff1), 0.6, 1, 1, 100, true);
-
 		this.offset = this.w / 2;
-		noStroke();
-		fill(this.hue, 0, 0, this.alpha / 2);
-		ellipse(this.x - this.offset, this.y - this.offset, this.w, this.w);
-		fill(0, 100, 100, this.alpha);
-		ellipse(this.x - this.offset, this.y + this.offset, this.w, this.w);
-		fill(200, 100, 100, this.alpha);
-		ellipse(this.x + this.offset, this.y - this.offset, this.w, this.w);
-		fill(this.hue, 0, 100, this.alpha);
-		ellipse(this.x + this.offset, this.y + this.offset, this.w * 1.25, this.w * 1.25);
 	}
 }
