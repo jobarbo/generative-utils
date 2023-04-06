06bb80f1d8b56f351e6b95d2a44cb8c165cb1aed
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-04-06 18:26:02 -0400

    new save png name

--- utils/saveCanvas.js
+++ utils/saveCanvas.js
@@ -1,9 +1,6 @@
 // if cmd + s is pressed, save the canvas'
-
 function keyPressed() {
-	console.log('key pressed', key);
 	if (key == 's' && (keyIsDown(91) || keyIsDown(93))) {
-		console.log('saving canvas');
 		saveArtwork();
 		// prevent the browser from saving the page
 		return false;
@@ -31,15 +28,11 @@ function saveArtwork() {
 	var datestring =
 		d.getDate() +
 		'_' +
-		(d.getMonth() + 1) +
+		`${d.getMonth() + 1}` +
 		'_' +
 		d.getFullYear() +
 		'_' +
-		d.getHours() +
-		'_' +
-		d.getMinutes() +
-		'_' +
-		d.getSeconds();
+		`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
 	var fileName = datestring + '.png';
 
 	save(fileName);
