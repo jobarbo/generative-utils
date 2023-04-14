16371aa46160caa39eed3b1c03d7295305900d20
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-04-14 16:03:57 -0400

    Update noise function from piter

--- utils/piterNoise.js
+++ utils/piterNoise.js
@@ -1,23 +1,69 @@
-// ok ignore the first bit of all this code, it defines the noise
-// function. I can explain how it works another day if you like.
+// Definitions ===========================================================
+({sin, cos, imul, PI} = Math);
+TAU = PI * 2;
+F = (N, f) => [...Array(N)].map((_, i) => f(i)); // for loop / map / list function
 
-let F = (N, f) => [...Array(N)].map((_) => f()); // loop function
-let imul = Math.imul,
-	floor = Math.floor;
-// 2d value noise function
-const KNUTH = 0x9e3779b1;
-let NSEED = Math.random() * 2 ** 32;
-let ri = (i, j, k) => (
+// A seeded PRNG =========================================================
+seed = 'Twelve Bored Monkeys'; // random seed
+S = Uint32Array.of(9, 7, 5, 3); // PRNG state
+R = (a = 1) =>
+	a *
+	((a = S[3]),
+	(S[3] = S[2]),
+	(S[2] = S[1]),
+	(a ^= a << 11),
+	(S[0] ^= a ^ (a >>> 8) ^ ((S[1] = S[0]) >>> 19)),
+	S[0] / 2 ** 32); // random function
+[...(seed + 'ThxPiter')].map((c) => R((S[3] ^= c.charCodeAt() * 23205))); // seeding the random function
+
+// general noise definitions =============================================
+KNUTH = 0x9e3779b1; // prime number close to PHI * 2 ** 32
+NSEED = R(2 ** 32); // noise seed, random 32 bit integer
+// 3d noise grid function
+ri = (i, j, k) => (
 	(i = imul((((i & 1023) << 20) | ((j & 1023) << 10) | ((i ^ j ^ k) & 1023)) ^ NSEED, KNUTH)),
 	(i <<= 3 + (i >>> 29)),
 	(i >>> 1) / 2 ** 31 - 0.5
 );
-let na = F(99, (_) => Math.random() * Math.PI * 2);
-let ns = na.map(Math.sin),
-	nc = na.map(Math.cos);
-let nox = F(99, (_) => Math.random() * 1024);
-let noy = F(99, (_) => Math.random() * 1024);
-let nz = (
+
+// 3D value noise function ===============================================
+no = F(99, (_) => R(1024)); // random noise offsets
+
+n3 = (
+	x,
+	y,
+	z,
+	s,
+	i, // (x,y,z) = coordinate, s = scale, i = noise offset index
+	xi = floor((x = x * s + no[(i *= 3)])), // (xi,yi,zi) = integer coordinates
+	yi = floor((y = y * s + no[i + 1])),
+	zi = floor((z = z * s + no[i + 2]))
+) => (
+	(x -= xi),
+	(y -= yi),
+	(z -= zi), // (x,y,z) are now fractional parts of coordinates
+	(x *= x * (3 - 2 * x)), // smoothstep polynomial (comment out if true linear interpolation is desired)
+	(y *= y * (3 - 2 * y)), // this is like an easing function for the fractional part
+	(z *= z * (3 - 2 * z)),
+	// calculate the interpolated value
+	ri(xi, yi, zi) * (1 - x) * (1 - y) * (1 - z) +
+		ri(xi, yi, zi + 1) * (1 - x) * (1 - y) * z +
+		ri(xi, yi + 1, zi) * (1 - x) * y * (1 - z) +
+		ri(xi, yi + 1, zi + 1) * (1 - x) * y * z +
+		ri(xi + 1, yi, zi) * x * (1 - y) * (1 - z) +
+		ri(xi + 1, yi, zi + 1) * x * (1 - y) * z +
+		ri(xi + 1, yi + 1, zi) * x * y * (1 - z) +
+		ri(xi + 1, yi + 1, zi + 1) * x * y * z
+);
+
+// 2D value noise function ===============================================
+na = F(99, (_) => R(TAU)); // random noise angles
+ns = na.map(sin);
+nc = na.map(cos); // sin and cos of those angles
+nox = F(99, (_) => R(1024)); // random noise x offset
+noy = F(99, (_) => R(1024)); // random noise y offset
+
+n2 = (
 	x,
 	y,
 	s,
@@ -25,7 +71,7 @@ let nz = (
 	c = nc[i] * s,
 	n = ns[i] * s,
 	xi = floor((([x, y] = [x * c + y * n + nox[i], y * c - x * n + noy[i]]), x)),
-	yi = floor(y)
+	yi = floor(y) // (x,y) = coordinate, s = scale, i = noise offset index
 ) => (
 	(x -= xi),
 	(y -= yi),
@@ -50,72 +96,72 @@ let nz = (
 // ohh also important to mention that it returns smooth noise values
 // between -.5 and .5
 
-function n1(x, y, s, i) {
+function oct1(x, y, s, i) {
 	// this function adds together 1 noise, in "octaves". This means
 	// it adds the first noise normally
-	return nz(x, y, s, i);
+	return n2(x, y, s, i);
 	// the result of this is that you get a better quality "cloudy" kind
 	// of noise, often called fBm ("fractal Brownian motion"). It is also
 	// often confused with Perlin noise but it's not.
 }
 
-function n2(x, y, s, i) {
+function oct2(x, y, s, i) {
 	// this function adds together 2 noises, in "octaves". This means
 	// it adds the first noise normally, and the second noise has double the scale but half the amplitude
 	i *= 2; // multiply the noise index by 2 because we use two noises
-	return nz(x, y, s, i) + nz(x, y, s * 2, i + 1) / 2;
+	return n2(x, y, s, i) + n2(x, y, s * 2, i + 1) / 2;
 	// the result of this is that you get a better quality "cloudy" kind
 	// of noise, often called fBm ("fractal Brownian motion"). It is also
 	// often confused with Perlin noise but it's not.
 }
 
-function n3(x, y, s, i) {
+function oct3(x, y, s, i) {
 	// this function adds together 3 noises, in "octaves". This means
 	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
 	i *= 3; // multiply the noise index by 3 because we use three noises
-	return nz(x, y, s, i) + nz(x, y, s * 2, i + 1) / 2 + nz(x, y, s * 4, i + 2) / 4;
+	return n2(x, y, s, i) + n2(x, y, s * 2, i + 1) / 2 + n2(x, y, s * 4, i + 2) / 4;
 	// the result of this is that you get a better quality "cloudy" kind
 	// of noise, often called fBm ("fractal Brownian motion"). It is also
 	// often confused with Perlin noise but it's not.
 }
 
-function n4(x, y, s, i) {
+function oct4(x, y, s, i) {
 	// this function adds together 3 noises, in "octaves". This means
 	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
 	i *= 4; // multiply the noise index by 3 because we use three noises
-	return nz(x, y, s, i) + nz(x, y, s * 2, i + 1) / 2 + nz(x, y, s * 4, i + 2) / 4 + nz(x, y, s * 8, i + 3) / 8;
+	return n2(x, y, s, i) + n2(x, y, s * 2, i + 1) / 2 + n2(x, y, s * 4, i + 2) / 4 + n2(x, y, s * 8, i + 3) / 8;
 	// the result of this is that you get a better quality "cloudy" kind
 	// of noise, often called fBm ("fractal Brownian motion"). It is also
 	// often confused with Perlin noise but it's not.
 }
 
-function n5(x, y, s, i) {
+function oct5(x, y, s, i) {
 	// this function adds together 3 noises, in "octaves". This means
 	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
 	i *= 5; // multiply the noise index by 3 because we use three noises
 	return (
-		nz(x, y, s, i) +
-		nz(x, y, s * 2, i + 1) / 2 +
-		nz(x, y, s * 4, i + 2) / 4 +
-		nz(x, y, s * 8, i + 3) / 8 +
-		nz(x, y, s * 16, i + 4) / 16
+		n2(x, y, s, i) +
+		n2(x, y, s * 2, i + 1) / 2 +
+		n2(x, y, s * 4, i + 2) / 4 +
+		n2(x, y, s * 8, i + 3) / 8 +
+		n2(x, y, s * 16, i + 4) / 16
 	);
 	// the result of this is that you get a better quality "cloudy" kind
 	// of noise, often called fBm ("fractal Brownian motion"). It is also
 	// often confused with Perlin noise but it's not.
 }
 
-function n6(x, y, s, i) {
+function oct6(x, y, s, i) {
 	// this function adds together 3 noises, in "octaves". This means
 	// it adds the first noise normally, the second noise has double the scale but half the amplitude, and the third noise has four times the scale and a quarter of the amplitude (if you want to add more it would be 8, 16, 32, etc)
 	i *= 6; // multiply the noise index by 3 because we use three noises
 	return (
-		nz(x, y, s, i) +
-		nz(x, y, s * 2, i + 1) / 2 +
-		nz(x, y, s * 4, i + 2) / 4 +
-		nz(x, y, s * 8, i + 3) / 8 +
-		nz(x, y, s * 16, i + 4) / 16 +
-		nz(x, y, s * 32, i + 5) / 32
+		n2(x, y, s, i) +
+		n2(x, y, s * 2, i + 1) / 2 +
+		n2(x, y, s * 4, i + 2) / 4 +
+		n2(x, y, s * 8, i + 3) / 8 +
+		n2(x, y, s * 16, i + 4) / 16 +
+		n2(x, y, s * 32, i + 5) / 32
 	);
 	// the result of this is that you get a better quality "cloudy" kind
 	// of noise, often called fBm ("fractal Brownian motion"). It is also
