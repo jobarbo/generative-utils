8ce15e2b91b59934f70ccf63915fee9b17962d13
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-10-25 23:11:49 -0400

    added function to utils

--- utils/utils.js
+++ utils/utils.js
@@ -17,13 +17,27 @@ function weighted_choice(data) {
 	return data[data.length - 1][0];
 }
 
-const mapValue = (v, cl, cm, tl, th, c) =>
-	c ? Math.min(Math.max(((v - cl) / (cm - cl)) * (th - tl) + tl, tl), th) : ((v - cl) / (cm - cl)) * (th - tl) + tl;
-
+let mapValue = (v, s, S, a, b) => ((v = Math.min(Math.max(v, s), S)), ((v - s) * (b - a)) / (S - s) + a);
 let clamp = (x, a, b) => (x < a ? a : x > b ? b : x);
 let smoothstep = (a, b, x) => (((x -= a), (x /= b - a)) < 0 ? 0 : x > 1 ? 1 : x * x * (3 - 2 * x));
 let mix = (a, b, p) => a + p * (b - a);
 let dot = (v1, v2) => v1.x * v2.x + v1.y * v2.y;
+
+let R = (a = 1) => Math.random() * a;
+let L = (x, y) => (x * x + y * y) ** 0.5; // Elements by Euclid 300 BC
+let k = (a, b) => (a > 0 && b > 0 ? L(a, b) : a > b ? a : b);
+
+function sdf_box([x, y], [cx, cy], [w, h]) {
+	x -= cx;
+	y -= cy;
+	return k(abs(x) - w, abs(y) - h);
+}
+
+function sdf_circle([x, y], [cx, cy], r) {
+	x -= cx;
+	y -= cy;
+	return L(x, y) - r;
+}
 
 let dpi = (maxDPI = 3.0) => {
 	let formatMode = features.format_mode;
