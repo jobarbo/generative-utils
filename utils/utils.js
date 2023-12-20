9cd296de5f479aa1e23aebd10614ccb90a7c3667
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-12-19 21:44:57 -0500

    moved noise and save to utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,131 @@
+let noiseCanvasWidth = 1;
+let noiseCanvasHeight = 1;
+
+// Definitions ===========================================================
+({sin, cos, imul, PI} = Math);
+TAU = PI * 2;
+F = (N, f) => [...Array(N)].map((_, i) => f(i)); // for loop / map / list function
+
+// A seeded PRNG =========================================================
+//seed = 'das9d7as9d7as'; // random seed]
+//seed = Math.random() * 2 ** 32;
+
+S = Uint32Array.of(9, 7, 5, 3); // PRNG state
+R = (a = 1) =>
+	a *
+	((a = S[3]),
+	(S[3] = S[2]),
+	(S[2] = S[1]),
+	(a ^= a << 11),
+	(S[0] ^= a ^ (a >>> 8) ^ ((S[1] = S[0]) >>> 19)),
+	S[0] / 2 ** 32); // random function
+[...(seed + 'ThxPiter')].map((c) => R((S[3] ^= c.charCodeAt() * 23205))); // seeding the random function
+
+// general noise definitions =============================================
+KNUTH = 0x9e3779b1; // prime number close to PHI * 2 ** 32
+NSEED = R(2 ** 32); // noise seed, random 32 bit integer
+// 3d noise grid function
+ri = (i, j, k) => (
+	(i = imul((((i & 1023) << 20) | ((j & 1023) << 10) | ((i ^ j ^ k) & 1023)) ^ NSEED, KNUTH)),
+	(i <<= 3 + (i >>> 29)),
+	(i >>> 1) / 2 ** 31 - 0.5
+);
+
+// 3D value noise function ===============================================
+no = F(99, (_) => R(1024)); // random noise offsets
+
+n3 = (
+	x,
+	y,
+	z,
+	s,
+	i, // (x,y,z) = coordinate, s = scale, i = noise offset index
+	xi = floor((x = x * s + no[(i *= 3)])), // (xi,yi,zi) = integer coordinates
+	yi = floor((y = y * s + no[i + 1])),
+	zi = floor((z = z * s + no[i + 2]))
+) => (
+	(x -= xi),
+	(y -= yi),
+	(z -= zi), // (x,y,z) are now fractional parts of coordinates
+	(x *= x * (3 - 2 * x)), // smoothstep polynomial (comment out if true linear interpolation is desired)
+	(y *= y * (3 - 2 * y)), // this is like an easing function for the fractional part
+	(z *= z * (3 - 2 * z)),
+	// calculate the interpolated value
+	ri(xi, yi, zi) * (1 - x) * (1 - y) * (1 - z) +
+		ri(xi, yi, zi + 1) * (1 - x) * (1 - y) * z +
+		ri(xi, yi + 1, zi) * (1 - x) * y * (1 - z) +
+		ri(xi, yi + 1, zi + 1) * (1 - x) * y * z +
+		ri(xi + 1, yi, zi) * x * (1 - y) * (1 - z) +
+		ri(xi + 1, yi, zi + 1) * x * (1 - y) * z +
+		ri(xi + 1, yi + 1, zi) * x * y * (1 - z) +
+		ri(xi + 1, yi + 1, zi + 1) * x * y * z
+);
+
+// 2D value noise function ===============================================
+na = F(99, (_) => R(TAU)); // random noise angles
+ns = na.map(sin);
+nc = na.map(cos); // sin and cos of those angles
+nox = F(99, (_) => R(1024)); // random noise x offset
+noy = F(99, (_) => R(1024)); // random noise y offset
+
+n2 = (
+	x,
+	y,
+	s,
+	i,
+	c = nc[i] * s,
+	n = ns[i] * s,
+	xi = floor(
+		(([x, y] = [
+			(x - noiseCanvasWidth / 2) * c + (y - noiseCanvasHeight * 2) * n + nox[i],
+			(y - noiseCanvasHeight * 2) * c - (x - noiseCanvasWidth / 2) * n + noy[i],
+		]),
+		x)
+	),
+	yi = floor(y) // (x,y) = coordinate, s = scale, i = noise offset index
+) => (
+	(x -= xi),
+	(y -= yi),
+	(x *= x * (3 - 2 * x)),
+	(y *= y * (3 - 2 * y)),
+	ri(xi, yi, i) * (1 - x) * (1 - y) +
+		ri(xi, yi + 1, i) * (1 - x) * y +
+		ri(xi + 1, yi, i) * x * (1 - y) +
+		ri(xi + 1, yi + 1, i) * x * y
+);
+
+//! Spell formula from Piter The Mage
+ZZ = (x, m, b, r) =>
+	x < 0
+		? x
+		: x > (b *= r * 4)
+		? x - b
+		: ((x /= r), fract(x / 4) < 0.5 ? r : -r) *
+		  ((x = abs(fract(x / 2) - 0.5)), 1 - (x > m ? x * 2 : x * (x /= m) * x * (2 - x) + m));
+
+// the point of all the previous code is that now you have a very
+// fast value noise function called nz(x,y,s,i). It has four parameters:
+// x -- the x coordinate
+// y -- the y coordinate
+// s -- the scale (simply multiplies x and y by s)
+// i -- the noise index, you get 99 different random noises! (but you
+//      can increase this number by changing the 99s in the code above)
+//      each of the 99 noises also has a random rotation which increases
+//      the "randomness" if you add many together
+//
+// ohh also important to mention that it returns smooth noise values
+// between -.5 and .5
+
+function oct(x, y, s, i, octaves = 1) {
+	let result = 0;
+	let sm = 1;
+	i *= octaves;
+	for (let j = 0; j < octaves; j++) {
+		result += n2(x, y, s * sm, i + j) / sm;
+		sm *= 2;
+	}
+	return result;
+}
 
 function weighted_choice(data) {
 	let total = 0;
@@ -48,3 +176,43 @@ let dpi = (maxDPI = 3.0) => {
 		return maxDPI;
 	}
 };
+
+// if cmd + s is pressed, save the canvas'
+function saveCanvas(event) {
+	if (event.key === "s" && (event.metaKey || event.ctrlKey)) {
+		saveArtwork();
+		// Prevent the browser from saving the page
+		event.preventDefault();
+		return false;
+	}
+}
+
+// Example usage to add an event listener for key presses
+document.addEventListener("keydown", saveCanvas);
+
+// make a function to save the canvas as a png file with the git branch name and a timestamp
+function saveArtwork() {
+	var dom_spin = document.querySelector(".spin-container");
+	var canvas = document.getElementById("defaultCanvas0");
+	var d = new Date();
+	var datestring =
+		d.getDate() +
+		"_" +
+		`${d.getMonth() + 1}` +
+		"_" +
+		d.getFullYear() +
+		"_" +
+		`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+	console.log(canvas);
+	var fileName = datestring + ".png";
+	const imageUrl = canvas
+		.toDataURL("image/png")
+		.replace("image/png", "image/octet-stream");
+	const a = document.createElement("a");
+	a.href = imageUrl;
+	a.setAttribute("download", fileName);
+	a.click();
+
+	dom_spin.classList.remove("active");
+	console.log("saved " + fileName);
+}
