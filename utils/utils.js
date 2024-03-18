7c342405f393c8a426a3e783a073ccb79f07d510
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-03-18 00:03:54 -0400

    updated saved img name

--- utils/utils.js
+++ utils/utils.js
@@ -201,7 +201,7 @@ function saveArtwork() {
 	var dom_spin = document.querySelector(".spin-container");
 	var canvas = document.getElementById("defaultCanvas0");
 	var d = new Date();
-	var datestring = d.getDate() + "_" + `${d.getMonth() + 1}` + "_" + d.getFullYear() + "_" + `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+	var datestring = `${d.getMonth() + 1}` + "_" + d.getDate() + "_" + d.getFullYear() + "_" + `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
 	console.log(canvas);
 	var fileName = datestring + ".png";
 	const imageUrl = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
