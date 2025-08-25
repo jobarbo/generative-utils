1759dfb980fa6d0d2201531ae540fc7f092f0bf3
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-08-25 18:36:09 -0400

    resolution tests

--- utils/shaderPipeline.js
+++ utils/shaderPipeline.js
@@ -23,7 +23,7 @@ class ShaderPipeline {
 			this.buffers = [];
 		} else {
 			// Only create 2 buffers when we actually need ping-pong (2+ effects)
-			this.buffers = [this.shaderManager.createBuffer(width / 5, height / 5), this.shaderManager.createBuffer(width / 5, height / 5)];
+			this.buffers = [this.shaderManager.createBuffer(width / 2, height / 2), this.shaderManager.createBuffer(width / 2, height / 2)];
 		}
 
 		// Initialize buffers
